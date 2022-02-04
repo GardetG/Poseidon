04fb7f10960dab479330d1190c02dc1dc3229600
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-04 18:07:20 +0100

    Add BidListService findAll() unit tests

---
+++ src/test/java/com/nnk/springboot/services/BidListServiceTest.java
@@ -0,0 +1,67 @@
+package com.nnk.springboot.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.dto.BidListDto;
+import com.nnk.springboot.repositories.BidListRepository;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class BidListServiceTest {
+
+  @Autowired
+  private BidListService bidListService;
+
+  @MockBean
+  private BidListRepository bidListRepository;
+
+  private BidList bidListTest;
+  private BidListDto bidListDtoTest;
+
+  @BeforeEach
+  void setUp() {
+    bidListTest = new BidList("Account Test", "Type Test", 10d);
+    bidListDtoTest = new BidListDto(1,"Account Test", "Type Test", 10d);
+  }
+
+  @DisplayName("Find all should return a list of BidListDto")
+  @Test
+  void findAllTest() {
+    // GIVEN
+    bidListTest.setBidListId(1);
+    when(bidListRepository.findAll()).thenReturn(Collections.singletonList(bidListTest));
+
+    // WHEN
+    List<BidListDto> actualDtos = bidListService.findAll();
+
+    // THEN
+    assertThat(actualDtos).usingRecursiveFieldByFieldElementComparator().containsExactly(bidListDtoTest);
+    verify(bidListRepository, times(1)).findAll();
+  }
+
+  @DisplayName("Find all when no BidList in database should return an empty list")
+  @Test
+  void findAllWhenEmptyTest() {
+    // GIVEN
+    when(bidListRepository.findAll()).thenReturn(Collections.emptyList());
+
+    // WHEN
+    List<BidListDto> actualDtos = bidListService.findAll();
+
+    // THEN
+    assertThat(actualDtos).isEmpty();
+    verify(bidListRepository, times(1)).findAll();
+  }
+}
