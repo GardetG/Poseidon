2732360b503b6c8d6c3443e7e406605980458a88
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-05 15:08:31 +0100

    Add BidListController GET and POST /bidList/add unit tests

--- src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
+++ src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
@@ -1,7 +1,13 @@
 package com.nnk.springboot.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -13,9 +19,12 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(value = BidListController.class)
@@ -26,6 +35,9 @@ class BidListControllerTest {
 
   @MockBean
   private BidListService bidListService;
+
+  @Captor
+  private ArgumentCaptor<BidListDto> dtoCaptor;
 
   @DisplayName("GET /bidList/list should return view with list of BidList as attribute")
   @Test
@@ -55,10 +67,64 @@ class BidListControllerTest {
     // WHEN
     mockMvc.perform(get("/bidList/list"))
 
-        // THEN
+    // THEN
         .andExpect(status().isOk())
         .andExpect(view().name("bidList/list"))
         .andExpect(model().attributeExists("bidLists"))
         .andExpect(model().attribute("bidLists", Collections.emptyList()));
   }
+
+  @DisplayName("GET /bidList/add should return view")
+  @Test
+  void addBidFormTest() throws Exception {
+    // WHEN
+    mockMvc.perform(get("/bidList/add"))
+
+    // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("bidList/add"));
+  }
+
+  @DisplayName("POST valid DTO on /bidList/validate should persist BidList then return view")
+  @Test
+  void validateTest() throws Exception {
+    // GIVEN
+    BidListDto expectedDto = new BidListDto(null,"Account Test","Type Test", 10d);
+
+    // WHEN
+    mockMvc.perform(post("/bidList/validate")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("account","Account Test")
+            .param("type", "Type Test")
+            .param("bidQuantity", String.valueOf(10d))
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isFound())
+        .andExpect(view().name("redirect:/bidList/list"));
+    verify(bidListService, times(1)).add(dtoCaptor.capture());
+    assertThat(dtoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expectedDto);
+  }
+
+  @DisplayName("POST invalid DTO on /bidList/validate should return same view with ")
+  @Test
+  void validateWhenInvalidTest() throws Exception {
+
+    // WHEN
+    mockMvc.perform(post("/bidList/validate")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("account","Account Test exceed 30 characters")
+            .param("type", "")
+            .param("bidQuantity", String.valueOf(-10d))
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("bidList/add"))
+        .andExpect(model().attributeHasFieldErrors("bidListDto", "account"))
+        .andExpect(model().attributeHasFieldErrors("bidListDto", "type"))
+        .andExpect(model().attributeHasFieldErrors("bidListDto", "bidQuantity"));
+    verify(bidListService, times(0)).add(any(BidListDto.class));
+  }
+
 }
