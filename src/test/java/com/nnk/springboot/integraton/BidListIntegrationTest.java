6dcc44662c714e58ed963ef5c3bbdac97cc0ed65
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-06 14:01:40 +0100

    Set Up H2 database for integration tests

--- src/test/java/com/nnk/springboot/integraton/BidListIntegrationTest.java
+++ src/test/java/com/nnk/springboot/integraton/BidListIntegrationTest.java
@@ -9,13 +9,17 @@ import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class BidListIntegrationTest {
 
 	@Autowired
