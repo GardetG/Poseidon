0119388edf2267d4de9b77cd77a60531a29105d5
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-06 17:04:19 +0100

    Add BidListService findById() unit tests

--- src/test/java/com/nnk/springboot/services/CurvePointServiceTest.java
+++ src/test/java/com/nnk/springboot/services/CurvePointServiceTest.java
@@ -1,15 +1,19 @@
 package com.nnk.springboot.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.dto.CurvePointDto;
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.repositories.CurvePointRepository;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -62,6 +66,35 @@ class CurvePointServiceTest {
     // THEN
     assertThat(actualDtos).isEmpty();
     verify(curvePointRepository, times(1)).findAll();
+  }
+
+  @DisplayName("Find by id should return the corresponding curvePointDto")
+  @Test
+  void findByIdTest() {
+    // GIVEN
+    when(curvePointRepository.findById(anyInt())).thenReturn(Optional.of(curvePointTest));
+
+    // WHEN
+    CurvePointDto actualDto = curvePointService.findById(1);
+
+    // THEN
+    assertThat(actualDto).usingRecursiveComparison().isEqualTo(curvePointDtoTest);
+    verify(curvePointRepository, times(1)).findById(1);
+  }
+
+  @DisplayName("Find by id when the corresponding curvePoint is not found should throw an exception")
+  @Test
+  void findByIdWhenNotFoundTest() {
+    // GIVEN
+    when(curvePointRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+    // WHEN
+    assertThatThrownBy(() -> curvePointService.findById(9))
+
+    // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessage("This curvePoint is not found");
+    verify(curvePointRepository, times(1)).findById(9);
   }
 
 }
