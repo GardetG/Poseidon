6c66cb11a901d055e798e019434853d69f5286a0
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-06 19:33:57 +0100

    Refactor CurvePointTests into CurvePointIntegrationTest

---
+++ src/test/java/com/nnk/springboot/integraton/CurvePointIntegrationTest.java
@@ -0,0 +1,57 @@
+package com.nnk.springboot.integraton;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class CurvePointIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private CurvePointRepository curvePointRepository;
+
+	@Test
+	void curvePointTest() {
+		CurvePoint curvePoint = new CurvePoint(10, 10d, 30d);
+
+		// Save
+		curvePoint = curvePointRepository.save(curvePoint);
+		assertNotNull(curvePoint.getId());
+		assertEquals(10, curvePoint.getCurveId());
+
+		// Update
+		curvePoint.setCurveId(20);
+		curvePoint = curvePointRepository.save(curvePoint);
+		assertEquals(20, curvePoint.getCurveId());
+
+		// Find
+		List<CurvePoint> listResult = curvePointRepository.findAll();
+		assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = curvePoint.getId();
+		curvePointRepository.delete(curvePoint);
+		Optional<CurvePoint> curvePointList = curvePointRepository.findById(id);
+		assertFalse(curvePointList.isPresent());
+	}
+
+}
