fadf2cae2431d44a0716c6da27cc886b75fb57b6
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-06 22:08:37 +0100

    Add RatingService findById() unit tests

--- src/test/java/com/nnk/springboot/services/RatingServiceTest.java
+++ src/test/java/com/nnk/springboot/services/RatingServiceTest.java
@@ -1,15 +1,20 @@
 package com.nnk.springboot.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.nnk.springboot.domain.Rating;
 import com.nnk.springboot.dto.RatingDto;
+import com.nnk.springboot.dto.RatingDto;
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.repositories.RatingRepository;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -64,5 +69,34 @@ class RatingServiceTest {
     assertThat(actualDtoList).isEmpty();
     verify(ratingRepository, times(1)).findAll();
   }
+  
+  @DisplayName("Find by id should return the corresponding RatingDto")
+  @Test
+  void findByIdTest() throws ResourceNotFoundException {
+    // GIVEN
+    when(ratingRepository.findById(anyInt())).thenReturn(Optional.of(ratingTest));
 
+    // WHEN
+    RatingDto actualDto = ratingService.findById(1);
+
+    // THEN
+    assertThat(actualDto).usingRecursiveComparison().isEqualTo(ratingDtoTest);
+    verify(ratingRepository, times(1)).findById(1);
+  }
+
+  @DisplayName("Find by id when the corresponding Rating is not found should throw an exception")
+  @Test
+  void findByIdWhenNotFoundTest() {
+    // GIVEN
+    when(ratingRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+    // WHEN
+    assertThatThrownBy(() -> ratingService.findById(9))
+
+        // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessage("This rating is not found");
+    verify(ratingRepository, times(1)).findById(9);
+  }
+  
 }
