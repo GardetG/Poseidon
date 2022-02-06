18209843bb623a6e39c9b308f6159a5c41c94156
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-06 18:36:21 +0100

    Add CurveController GET and POST /curvePoint/add unit tests

--- src/test/java/com/nnk/springboot/controllers/CurveControllerTest.java
+++ src/test/java/com/nnk/springboot/controllers/CurveControllerTest.java
@@ -1,11 +1,18 @@
 package com.nnk.springboot.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.nnk.springboot.dto.BidListDto;
 import com.nnk.springboot.dto.CurvePointDto;
 import com.nnk.springboot.services.CurvePointService;
 import java.util.ArrayList;
@@ -13,9 +20,12 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(CurveController.class)
@@ -26,6 +36,9 @@ class CurveControllerTest {
 
   @MockBean
   private CurvePointService curvePointService;
+
+  @Captor
+  ArgumentCaptor<CurvePointDto> dtoCaptor;
 
   @DisplayName("GET /curvePoint/list should return view with list of CurvePoint as attribute")
   @Test
@@ -60,6 +73,56 @@ class CurveControllerTest {
         .andExpect(view().name("curvePoint/list"))
         .andExpect(model().attributeExists("curvePoints"))
         .andExpect(model().attribute("curvePoints", Collections.emptyList()));
+  }
+
+  @DisplayName("GET /curvePoint/add should return view")
+  @Test
+  void addBidFormTest() throws Exception {
+    // WHEN
+    mockMvc.perform(get("/curvePoint/add"))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("curvePoint/add"));
+  }
+
+  @DisplayName("POST valid DTO on /curvePoint/validate should persist CurvePoint then return view")
+  @Test
+  void validateTest() throws Exception {
+    // GIVEN
+    CurvePointDto expectedDto = new CurvePointDto(null,10,10d,30d);
+
+    // WHEN
+    mockMvc.perform(post("/curvePoint/validate")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("curveId", String.valueOf(expectedDto.getCurveId()))
+            .param("term", String.valueOf(expectedDto.getTerm()))
+            .param("value", String.valueOf(expectedDto.getValue()))
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isFound())
+        .andExpect(view().name("redirect:/curvePoint/list"));
+    verify(curvePointService, times(1)).add(dtoCaptor.capture());
+    assertThat(dtoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expectedDto);
+  }
+
+  @DisplayName("POST invalid DTO on /curvePoint/validate should return form view")
+  @Test
+  void validateWhenInvalidTest() throws Exception {
+    // WHEN
+    mockMvc.perform(post("/curvePoint/validate")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("curveId","")
+            .param("term", String.valueOf(10d))
+            .param("value", String.valueOf(30d))
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("curvePoint/add"))
+        .andExpect(model().attributeHasFieldErrors("curvePointDto", "curveId"));
+    verify(curvePointService, times(0)).add(any(CurvePointDto.class));
   }
 
 }
