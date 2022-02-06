1cd2a144fb6e90cb5b3eb64829de7b7d4a57483d
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-06 23:43:16 +0100

    Refactor RatingTests into RatingIntegrationTest

---
+++ src/test/java/com/nnk/springboot/integraton/RatingIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.nnk.springboot.integraton;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class RatingIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private RatingRepository ratingRepository;
+
+	@Test
+	void ratingTest() {
+		Rating rating = new Rating("Moodys Rating", "Sand PRating", "Fitch Rating", 10);
+
+		// Save
+		rating = ratingRepository.save(rating);
+		assertNotNull(rating.getId());
+		assertEquals(10, rating.getOrderNumber());
+
+		// Update
+		rating.setOrderNumber(20);
+		rating = ratingRepository.save(rating);
+		assertEquals(20, rating.getOrderNumber());
+
+		// Find
+		List<Rating> listResult = ratingRepository.findAll();
+		assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = rating.getId();
+		ratingRepository.delete(rating);
+		Optional<Rating> ratingList = ratingRepository.findById(id);
+		assertFalse(ratingList.isPresent());
+	}
+}
