76e92a715eeb8aa410d4d99a7a213c386a39bd02
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 11:13:31 +0100

    Add RuleNameService Logger and JavaDoc

--- src/main/java/com/nnk/springboot/services/RuleNameServiceImpl.java
+++ src/main/java/com/nnk/springboot/services/RuleNameServiceImpl.java
@@ -7,6 +7,8 @@ import com.nnk.springboot.repositories.RuleNameRepository;
 import com.nnk.springboot.utils.RuleNameMapper;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +17,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RuleNameServiceImpl implements RuleNameService {
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(RuleNameServiceImpl.class);
 
   @Autowired
   private RuleNameRepository ruleNameRepository;
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public List<RuleNameDto> findAll() {
     return ruleNameRepository.findAll()
@@ -27,11 +34,17 @@ public class RuleNameServiceImpl implements RuleNameService {
         .collect(Collectors.toList());
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public RuleNameDto findById(int id) throws ResourceNotFoundException {
     return RuleNameMapper.toDto(getOrThrowException(id));
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public void add(RuleNameDto ruleNameDto) {
     RuleName ruleNameToAdd = new RuleName();
@@ -39,6 +52,9 @@ public class RuleNameServiceImpl implements RuleNameService {
     ruleNameRepository.save(ruleNameToAdd);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public void update(RuleNameDto ruleNameDto) throws ResourceNotFoundException {
     RuleName ruleNameToUpdate = getOrThrowException(ruleNameDto.getId());
@@ -46,6 +62,9 @@ public class RuleNameServiceImpl implements RuleNameService {
     ruleNameRepository.save(ruleNameToUpdate);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public void delete(int id) throws ResourceNotFoundException {
     RuleName ruleNameToDelete = getOrThrowException(id);
@@ -54,7 +73,10 @@ public class RuleNameServiceImpl implements RuleNameService {
 
   private RuleName getOrThrowException(int id) throws ResourceNotFoundException {
     return ruleNameRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("This ruleName is not found"));
+        .orElseThrow(() -> {
+          LOGGER.error("The RuleName with id {} is not found", id);
+          return new ResourceNotFoundException("This ruleName is not found");
+        });
   }
   
 }
