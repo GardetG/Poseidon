37c953d784659f13360e98b3d2774485e0207319
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 11:35:49 +0100

    Add RuleNameController GET and POST /ruleName/update unit tests

--- src/test/java/com/nnk/springboot/controllers/RuleNameControllerTest.java
+++ src/test/java/com/nnk/springboot/controllers/RuleNameControllerTest.java
@@ -2,17 +2,21 @@ package com.nnk.springboot.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.nnk.springboot.dto.RuleNameDto;
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.services.RuleNameService;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -136,5 +140,116 @@ class RuleNameControllerTest {
         .andExpect(model().attributeHasFieldErrors("ruleNameDto", "sqlPart"));
     verify(ruleNameService, times(0)).add(any(RuleNameDto.class));
   }
-  
+
+  @DisplayName("GET /ruleName/update should return view")
+  @Test
+  void showUpdateFormTest() throws Exception {
+    // GIVEN
+    RuleNameDto ruleNameDto = new RuleNameDto(1, "Rule Name", "Description", "Json", "Template", "SQL", "SQL Part");
+    when(ruleNameService.findById(anyInt())).thenReturn(ruleNameDto);
+
+    // WHEN
+    mockMvc.perform(get("/ruleName/update/1"))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("ruleName/update"))
+        .andExpect(model().attributeExists("ruleNameDto"))
+        .andExpect(model().attribute("ruleNameDto", ruleNameDto));
+    verify(ruleNameService, times(1)).findById(1);
+  }
+
+  @DisplayName("GET /ruleName/update when ruleName not found should return view with error message")
+  @Test
+  void showUpdateFormWhenNotFoundTest() throws Exception {
+    // GIVEN
+    doThrow(new ResourceNotFoundException("This ruleName is not found")).when(ruleNameService).findById(anyInt());
+
+    // WHEN
+    mockMvc.perform(get("/ruleName/update/9"))
+
+        // THEN
+        .andExpect(status().isFound())
+        .andExpect(view().name("redirect:/ruleName/list"))
+        .andExpect(flash().attributeExists("error"));
+    verify(ruleNameService, times(1)).findById(9);
+  }
+
+  @DisplayName("POST valid DTO on /ruleName/update should persist ruleName then return view")
+  @Test
+  void updateCurveTest() throws Exception {
+    // GIVEN
+    RuleNameDto expectedDto = new RuleNameDto(1, "Update Rule Name", "Update Description", "Update Json", "Update Template", "Update SQL", "Update SQL Part");
+
+    // WHEN
+    mockMvc.perform(post("/ruleName/update/1")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("name", expectedDto.getName())
+            .param("description", expectedDto.getDescription())
+            .param("json", expectedDto.getJson())
+            .param("template", expectedDto.getTemplate())
+            .param("sqlStr", expectedDto.getSqlStr())
+            .param("sqlPart", expectedDto.getSqlPart())
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isFound())
+        .andExpect(view().name("redirect:/ruleName/list"));
+    verify(ruleNameService, times(1)).update(dtoCaptor.capture());
+    assertThat(dtoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expectedDto);
+  }
+
+  @DisplayName("POST invalid DTO on /ruleName/update should return from view")
+  @Test
+  void updateCurveWhenInvalidTest() throws Exception {
+    // WHEN
+    mockMvc.perform(post("/ruleName/update/1")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("name", "This ruleName's name exceed the 125 characters limit to test the validation of the Dto " +
+                "when the add RuleName form is submit by the user")
+            .param("description", "")
+            .param("json", "")
+            .param("template", "")
+            .param("sqlStr", "")
+            .param("sqlPart", "")
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("ruleName/update"))
+        .andExpect(model().attributeHasFieldErrors("ruleNameDto", "name"))
+        .andExpect(model().attributeHasFieldErrors("ruleNameDto", "description"))
+        .andExpect(model().attributeHasFieldErrors("ruleNameDto", "json"))
+        .andExpect(model().attributeHasFieldErrors("ruleNameDto", "template"))
+        .andExpect(model().attributeHasFieldErrors("ruleNameDto", "sqlStr"))
+        .andExpect(model().attributeHasFieldErrors("ruleNameDto", "sqlPart"));
+    verify(ruleNameService, times(0)).update(any(RuleNameDto.class));
+  }
+
+  @DisplayName("POST DTO on /ruleName/update when ruleName not found should return view with error message")
+  @Test
+  void updateCurveWhenNotFoundTest() throws Exception {
+    // GIVEN
+    RuleNameDto expectedDto = new RuleNameDto(9, "Update Rule Name", "Update Description", "Update Json", "Update Template", "Update SQL", "Update SQL Part");
+    doThrow(new ResourceNotFoundException("This ruleName is not found")).when(ruleNameService).update(any(RuleNameDto.class));
+
+    // WHEN
+    mockMvc.perform(post("/ruleName/update/9")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("name", expectedDto.getName())
+            .param("description", expectedDto.getDescription())
+            .param("json", expectedDto.getJson())
+            .param("template", expectedDto.getTemplate())
+            .param("sqlStr", expectedDto.getSqlStr())
+            .param("sqlPart", expectedDto.getSqlPart())
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isFound())
+        .andExpect(view().name("redirect:/ruleName/list"))
+        .andExpect(flash().attributeExists("error"));
+    verify(ruleNameService, times(1)).update(dtoCaptor.capture());
+    assertThat(dtoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expectedDto);
+  }
+
 }
