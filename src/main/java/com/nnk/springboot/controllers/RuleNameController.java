967da317e0af866cc867e575c002261b69650575
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 11:43:45 +0100

    Update RatingController GET /rating/delete method

--- src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -63,8 +63,9 @@ public class RuleNameController {
   }
 
   @GetMapping("/ruleName/delete/{id}")
-  public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-    // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+  public String deleteRuleName(@PathVariable("id") Integer id, Model model)
+      throws ResourceNotFoundException {
+    ruleNameService.delete(id);
     return "redirect:/ruleName/list";
   }
 
