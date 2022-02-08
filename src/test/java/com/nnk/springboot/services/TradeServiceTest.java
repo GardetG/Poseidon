0dabef420f7dea2de9f108d3ffc86fca12bef91e
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 13:24:04 +0100

    Add TradeService findById() unit tests

--- src/test/java/com/nnk/springboot/services/TradeServiceTest.java
+++ src/test/java/com/nnk/springboot/services/TradeServiceTest.java
@@ -1,15 +1,20 @@
 package com.nnk.springboot.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.dto.TradeDto;
+import com.nnk.springboot.dto.TradeDto;
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.repositories.TradeRepository;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -69,5 +74,34 @@ class TradeServiceTest {
     assertThat(actualDtoList).isEmpty();
     verify(tradeRepository, times(1)).findAll();
   }
+  
+  @DisplayName("Find by id should return the corresponding TradeDto")
+  @Test
+  void findByIdTest() throws ResourceNotFoundException {
+    // GIVEN
+    when(tradeRepository.findById(anyInt())).thenReturn(Optional.of(tradeTest));
 
+    // WHEN
+    TradeDto actualDto = tradeService.findById(1);
+
+    // THEN
+    assertThat(actualDto).usingRecursiveComparison().isEqualTo(tradeDtoTest);
+    verify(tradeRepository, times(1)).findById(1);
+  }
+
+  @DisplayName("Find by id when the corresponding Trade is not found should throw an exception")
+  @Test
+  void findByIdWhenNotFoundTest() {
+    // GIVEN
+    when(tradeRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+    // WHEN
+    assertThatThrownBy(() -> tradeService.findById(9))
+
+        // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessage("This trade is not found");
+    verify(tradeRepository, times(1)).findById(9);
+  }
+  
 }
