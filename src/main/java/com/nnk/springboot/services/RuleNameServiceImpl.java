3a26ba2dff3a19cfff6c4b28d6a3a370257d99d2
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 10:48:58 +0100

    Implement RuleNameService findAll() method

--- src/main/java/com/nnk/springboot/services/RuleNameServiceImpl.java
+++ src/main/java/com/nnk/springboot/services/RuleNameServiceImpl.java
@@ -1,16 +1,35 @@
 package com.nnk.springboot.services;
 
 import com.nnk.springboot.dto.RuleNameDto;
+import com.nnk.springboot.repositories.RuleNameRepository;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Service class implementation for RuleName entity CRUD operations.
  */
+@Service
 public class RuleNameServiceImpl implements RuleNameService {
+
+  @Autowired
+  private RuleNameRepository ruleNameRepository;
 
   @Override
   public List<RuleNameDto> findAll() {
-    return null;
+    return ruleNameRepository.findAll()
+        .stream()
+        .map(ruleName -> new RuleNameDto(
+            ruleName.getId(),
+            ruleName.getName(),
+            ruleName.getDescription(),
+            ruleName.getJson(),
+            ruleName.getTemplate(),
+            ruleName.getSqlStr(),
+            ruleName.getSqlPart()
+            )
+        ).collect(Collectors.toList());
   }
 
   @Override
