cf9cf067e0a3bb28f6a63ae2238dd9a59e17743c
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 13:35:12 +0100

    Refactor TradeService

--- src/main/java/com/nnk/springboot/services/TradeServiceImpl.java
+++ src/main/java/com/nnk/springboot/services/TradeServiceImpl.java
@@ -4,6 +4,7 @@ import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.dto.TradeDto;
 import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.repositories.TradeRepository;
+import com.nnk.springboot.utils.TradeMapper;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,50 +23,37 @@ public class TradeServiceImpl implements TradeService {
   public List<TradeDto> findAll() {
     return tradeRepository.findAll()
         .stream()
-        .map(trade -> new TradeDto(
-            trade.getTradeId(),
-            trade.getAccount(),
-            trade.getType(),
-            trade.getBuyQuantity()
-        ))
+        .map(TradeMapper::toDto)
         .collect(Collectors.toList());
   }
 
   @Override
   public TradeDto findById(int id) throws ResourceNotFoundException {
-    return tradeRepository.findById(id)
-        .map(trade -> new TradeDto(
-            trade.getTradeId(),
-            trade.getAccount(),
-            trade.getType(),
-            trade.getBuyQuantity()
-        ))
-        .orElseThrow(() -> new ResourceNotFoundException("This trade is not found"));
+    return TradeMapper.toDto(getOrThrowException(id));
   }
 
   @Override
   public void add(TradeDto tradeDto) {
-    Trade tradeToAdd = new Trade(
-        tradeDto.getAccount(),
-        tradeDto.getType(),
-        tradeDto.getBuyQuantity()
-    );
+    Trade tradeToAdd = new Trade();
+    TradeMapper.toEntity(tradeToAdd, tradeDto);
     tradeRepository.save(tradeToAdd);
   }
 
   @Override
   public void update(TradeDto tradeDto) throws ResourceNotFoundException {
-    Trade tradeToUpdate = tradeRepository.findById(tradeDto.getTradeId())
-        .orElseThrow(() -> new ResourceNotFoundException("This trade is not found"));
-    tradeToUpdate.setAccount(tradeDto.getAccount());
-    tradeToUpdate.setType(tradeDto.getType());
-    tradeToUpdate.setBuyQuantity(tradeDto.getBuyQuantity());
+    Trade tradeToUpdate = getOrThrowException(tradeDto.getTradeId());
+    TradeMapper.toEntity(tradeToUpdate, tradeDto);
     tradeRepository.save(tradeToUpdate);
   }
 
   @Override
   public void delete(int id) {
 
+  }
+
+  private Trade getOrThrowException(int id) throws ResourceNotFoundException {
+    return tradeRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("This trade is not found"));
   }
 
 }
