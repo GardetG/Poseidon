daceb21290d352e4637fd733dd1a84cd0e970ee6
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-08 10:50:35 +0100

    Add RuleNameService findById() unit tests

--- src/test/java/com/nnk/springboot/services/RuleNameServiceTest.java
+++ src/test/java/com/nnk/springboot/services/RuleNameServiceTest.java
@@ -1,15 +1,19 @@
 package com.nnk.springboot.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.dto.RuleNameDto;
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.repositories.RuleNameRepository;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -68,6 +72,35 @@ class RuleNameServiceTest {
     // THEN
     assertThat(actualDtoList).isEmpty();
     verify(ruleNameRepository, times(1)).findAll();
+  }
+  
+  @DisplayName("Find by id should return the corresponding RuleNameDto")
+  @Test
+  void findByIdTest() throws ResourceNotFoundException {
+    // GIVEN
+    when(ruleNameRepository.findById(anyInt())).thenReturn(Optional.of(ruleNameTest));
+
+    // WHEN
+    RuleNameDto actualDto = ruleNameService.findById(1);
+
+    // THEN
+    assertThat(actualDto).usingRecursiveComparison().isEqualTo(ruleNameDtoTest);
+    verify(ruleNameRepository, times(1)).findById(1);
+  }
+
+  @DisplayName("Find by id when the corresponding RuleName is not found should throw an exception")
+  @Test
+  void findByIdWhenNotFoundTest() {
+    // GIVEN
+    when(ruleNameRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+    // WHEN
+    assertThatThrownBy(() -> ruleNameService.findById(9))
+
+        // THEN
+        .isInstanceOf(ResourceNotFoundException.class)
+        .hasMessage("This ruleName is not found");
+    verify(ruleNameRepository, times(1)).findById(9);
   }
 
 }
