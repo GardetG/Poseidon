3d08f23db2f5caf7c12d2e84a2aa2a548da80d97
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-02-10 13:56:19 +0100

    Add UserController GET and POST /user/add unit tests

--- src/test/java/com/nnk/springboot/controllers/UserControllerTest.java
+++ src/test/java/com/nnk/springboot/controllers/UserControllerTest.java
@@ -1,17 +1,20 @@
 package com.nnk.springboot.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import com.nnk.springboot.dto.UserDto;
 import com.nnk.springboot.dto.UserDto;
 import com.nnk.springboot.exceptions.ResourceNotFoundException;
 import com.nnk.springboot.repositories.UserRepository;
@@ -21,9 +24,12 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
@@ -36,6 +42,9 @@ class UserControllerTest {
   private UserService userService;
   @MockBean
   private UserRepository userRepository;
+
+  @Captor
+  private ArgumentCaptor<UserDto> dtoCaptor;
 
   @DisplayName("GET /user/list should return view with list of User as attribute")
   @Test
@@ -54,6 +63,7 @@ class UserControllerTest {
         .andExpect(view().name("user/list"))
         .andExpect(model().attributeExists("users"))
         .andExpect(model().attribute("users", DtoList));
+    assertThat(DtoList).extracting("password").containsOnlyNulls();
   }
 
   @DisplayName("GET /user/list with no User in database should return view with empty list as attribute")
@@ -72,6 +82,63 @@ class UserControllerTest {
         .andExpect(model().attribute("users", Collections.emptyList()));
   }
 
+  @DisplayName("GET /user/add should return view")
+  @Test
+  void addUserFormTest() throws Exception {
+    // WHEN
+    mockMvc.perform(get("/user/add"))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("user/add"));
+  }
+
+  @DisplayName("POST valid DTO on /user/validate should persist User then return view")
+  @Test
+  void validateTest() throws Exception {
+    // GIVEN
+    UserDto expectedDto = new UserDto(null, "Username 1", "User 1", "USER");
+    expectedDto.setPassword("Password");
+
+    // WHEN
+    mockMvc.perform(post("/user/validate")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("username", expectedDto.getUsername())
+            .param("password", expectedDto.getPassword())
+            .param("fullName", expectedDto.getFullName())
+            .param("role", expectedDto.getRole())
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isFound())
+        .andExpect(view().name("redirect:/user/list"));
+    verify(userService, times(1)).add(dtoCaptor.capture());
+    assertThat(dtoCaptor.getValue()).usingRecursiveComparison().isEqualTo(expectedDto);
+  }
+
+  @DisplayName("POST invalid DTO on /user/validate should return form view")
+  @Test
+  void validateWhenInvalidTest() throws Exception {
+    // WHEN
+    mockMvc.perform(post("/user/validate")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("username","This user's username exceed the 125 characters limit " +
+                "to test the validation of the Dto when the add User form is submit by the user")
+            .param("password", "")
+            .param("fullName", "")
+            .param("role", "")
+            .with(csrf()))
+
+        // THEN
+        .andExpect(status().isOk())
+        .andExpect(view().name("user/add"))
+        .andExpect(model().attributeHasFieldErrors("userDto", "username"))
+        .andExpect(model().attributeHasFieldErrors("userDto", "password"))
+        .andExpect(model().attributeHasFieldErrors("userDto", "fullName"))
+        .andExpect(model().attributeHasFieldErrors("userDto", "role"));
+    verify(userService, times(0)).add(any(UserDto.class));
+  }
+
   @DisplayName("GET /user/update should return view")
   @Test
   void showUpdateFormTest() throws Exception {
@@ -88,6 +155,7 @@ class UserControllerTest {
         .andExpect(model().attributeExists("userDto"))
         .andExpect(model().attribute("userDto", userDto));
     verify(userService, times(1)).findById(1);
+    assertThat(userDto.getPassword()).isNull();
   }
 
   @DisplayName("GET /user/update when user not found should return view with error message")
